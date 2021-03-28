664fee750a31e9c15636f2f4cdc08be8af84c6cf
einpet <78473929+einpet@users.noreply.github.com>
2021-03-29 02:55:19 +0300

    Tvarkymai

--- Pravalas/resources/views/layouts/mainPage.blade.php
+++ Pravalas/resources/views/layouts/mainPage.blade.php
@@ -8,7 +8,53 @@
     <title>Pravalas</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<style>
+.LogButton{
+    box-shadow: 3px 3px 3px -3px rgba(0,0,0,0.75);
+    padding:2px 15.6px 2px 15.6px;
+    background-color: rgb(165, 7, 7);
+    color: white; 
+    font-weight:bold;
+}
+.RegButton{
+    box-shadow: 3px 3px 3px -3px rgba(0,0,0,0.75);
+    padding:2px 12.5px 2px 12.5px;
+    background-color: rgb(165, 7, 7);
+    color: white;
+}
+.Book{
+    box-shadow: 3px 3px 3px -3px rgba(0,0,0,0.75);
+    width:90px;
+    height:50px;
+    
+}
+.Search{
+    box-shadow: 3px 3px 3px -3px rgba(0,0,0,0.75);
+    border:3px;
+    margin-top:11px;
+    padding:8px 85px 8px 15px;
+    border-style:solid;
+    border-color:rgb(165, 7, 7);
+    resize:none;
+    color:grey;
+    font-style:italic;
+}
+.SearchButton{
+    box-shadow: 3px 3px 3px -3px rgba(0,0,0,0.75);
+    width:46.9px;
+    height:46.9px;
+}
+#logo{
+    width:70%;
+    margin:0 auto;
+    text-align:right;
+}
+.Navigation{
+    width:70%;
+    margin:0 auto;
+}
 
+</style>
 
 </head>
 
@@ -16,31 +62,33 @@
 <div id="logo">
     <nav class="p-6 bg-white flex justify-between">
         <ul style="align-items: center">
-        <a href="{{ 'http://127.0.0.1:8000/' }}"> <img style="margin-left: 0%" src="logo.png" width="250" height="250" /> </a>
+        <a href="http://127.0.0.1:8000/"> <img src="logo.png" width="270" > </a>
         </ul>
 
 
         <ul class="flex items-center">
             <li class="">
-                <textarea style="border:3px; border-style:solid; border-color:rgb(165, 7, 7); resize:none"
-                    name="Surasti Knygą" id="" cols="50" rows="1"> Ieškoti knygos pagal jos autorių, pavadinimą..</textarea>
+                <textarea class="Search"
+                    name="Surasti Knygą" id="" cols="50" rows="1"> Ieškoti knygos pagal jos autorių, pavadinimą.. </textarea>
                 <button style="margin-left:-2%">
-                    <img src="search.png" alt="" width="33.5px" height="33.5px">
+                    <img class="SearchButton" src="search.png" alt="">
                 </button>
             </li>
         </ul>
         <ul class="flex items-center">
-            <button style="margin-right: 5%">
-                <img src="book.png" alt="" width="50 px" height="50px">
+            <button style="margin-left: 44%;">
+                <img class="Book" src="book.png" alt="">
             </button>
             <li style="justify-items: flex-end">
                 
                 <a href="{{ 'login' }}"
-                style=" background-color: rgb(165, 7, 7);color: white; margin-right:80%;width:90%;">
+                class="LogButton";
+                >
                 Prisijungti
             </a>
                 <a href="{{ 'register' }}"
-                    style=" background-color: rgb(165, 7, 7);color: white; margin-right:80%;width:90%;">
+                    class="RegButton";
+                    >
                     Registruotis
                 </a>
             </li>
@@ -48,18 +96,20 @@
     </nav>
 </div>
     <nav class="p-3 bg-white  mb-1; ">
+   
+
         <h1 style="margin-top:0.2%; border-bottom: 1px solid lightgray"></h1>
-        <button style="margin-left:5%; color:grey;font-size: 20px">Naujai įdėtos knygos</button>
+         <div class="Navigation">
+        <button style="margin-left:10px; color:grey;font-size: 20px">Naujai įdėtos knygos</button>
         <button style="margin-left:1.5%;color:grey;font-size: 20px">Knygos</button>
         <button style="margin-left:1.5%;color:grey;font-size: 20px">Pagalba</button>
         <button style="margin-left:1.5%;color:grey;font-size: 20px ">Apie mus</button>
+        </div>
         <h1 style="margin-top:0.2%; border-bottom: 1px solid lightgray"></h1>
-    </nav>
-
+        
+   </nav>
+</div>
     @yield('MainPage')
 </body>
-
-
-
 
 </html>
--- Pravalas/resources/views/posts/main.blade.php
+++ Pravalas/resources/views/posts/main.blade.php
@@ -1,13 +1,25 @@
 @extends('layouts.mainPage')
 
 @section('MainPage')
-
+    <div style="width:68%; margin:0 auto; background-color:#F3F3F3;">
     <nav>
         <img style="margin:auto" src=" your.png" alt="">
     </nav>
+         <br>
+        <p style="margin:0 30px 0 30px; text-align:justify"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus purus sapien, porttitor eu enim nec, rhoncus ullamcorper turpis. Nullam bibendum elit in hendrerit faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus sit amet feugiat arcu, non fermentum dolor. Etiam ut tincidunt quam. Vestibulum venenatis sapien vestibulum, suscipit justo nec, efficitur ex. Nullam quam enim, rhoncus vitae commodo in, laoreet at leo. Morbi mollis urna sed accumsan tincidunt. Ut ac feugiat metus. Suspendisse turpis augue, egestas ut interdum id, aliquam in libero.
 
-    <footer style=" position: fixed;left: 0;bottom: 0;width: 100%;background-color: rgb(165, 7, 7);color: white;text-align:
-                    center;">
+Duis nec dui convallis, congue tortor ut, consectetur justo. Aenean ultrices tellus at nulla sodales, nec placerat ante porta. Aliquam lectus lectus, viverra ac pretium in, pharetra quis lectus. Aenean rutrum velit ac arcu tincidunt semper. Mauris sagittis lectus quis elementum mattis. Mauris sit amet ipsum urna. In commodo sem quis dapibus bibendum. Pellentesque efficitur sapien mi. Suspendisse dictum sodales nunc, non tristique risus condimentum non. Quisque eu dignissim magna. Vivamus lorem erat, elementum quis massa sit amet, semper gravida nisi. Nunc nec fermentum dolor, in tempor mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent felis enim, porttitor eu massa eu, vulputate rhoncus nunc. Donec eget risus efficitur, eleifend libero quis, commodo purus.
+
+Ut egestas ligula in libero facilisis, vel volutpat ex vulputate. Nunc nibh augue, consectetur vitae pretium ac, consectetur sed elit. Vestibulum id mauris ultricies, tempor lectus vel, molestie elit. Praesent non arcu id turpis sodales consequat non vel neque. Nunc nec tortor venenatis, ultrices ipsum nec, lobortis odio. Pellentesque dictum, nibh sit amet malesuada blandit, risus dolor porta magna, at mollis magna elit non mauris. Proin consequat dictum ullamcorper. Duis venenatis eu leo vitae rutrum. Maecenas hendrerit arcu ex, a gravida ante rhoncus ut. Morbi ac leo sed enim tincidunt cursus a non mauris. Integer varius arcu sit amet nisi venenatis euismod. Proin hendrerit auctor nunc eu gravida.
+
+Vestibulum suscipit molestie nulla in molestie. Vivamus mollis nisi dictum nisi sollicitudin imperdiet. Aliquam sed lacinia neque. Pellentesque quis felis at lorem dictum convallis a eu erat. Nullam maximus libero ut lacinia pulvinar. Fusce non sapien eu ipsum eleifend mollis. Cras euismod tempus libero, nec ultricies sem. Aliquam sit amet malesuada turpis. Nunc blandit neque non justo vestibulum blandit.
+
+Maecenas viverra massa in commodo malesuada. Proin porta vulputate lacus quis sollicitudin. Nunc cursus elit at justo finibus, faucibus semper nisi aliquam. Sed sagittis augue felis. Phasellus consequat magna sed ultrices eleifend. Morbi id efficitur lectus. Suspendisse potenti. In quis mauris nisi. Quisque sem felis, scelerisque in ligula porta, aliquet luctus enim. Integer lobortis dignissim turpis, at consequat ipsum varius at.</p>
+    </div>
+    <br>
+
+    <footer style="width: 100%;background-color: rgb(165, 7, 7);color: white;text-align:center;">
+        <div style="width:70%; margin:0 auto;">          
         <nav class="p-6 bg-rgb(165, 7, 7) flex justify-between mb-6; align-items:center">
             <ul style=" align-items: center; margin-left:5%; margin-top:1%">
                 <p> Kontaktai</p>
@@ -32,6 +44,7 @@
             </ul>
 
         </nav>
+        </div>
     </footer>
 
 @endsection
