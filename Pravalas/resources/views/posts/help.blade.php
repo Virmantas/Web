214ad29747a399106c9302329ff6f9145c27aaeb
Deividas77 <78477923+Deividas77@users.noreply.github.com>
2021-03-31 16:06:40 +0300

    pagalbos psl. pataisymas

--- Pravalas/resources/views/posts/help.blade.php
+++ Pravalas/resources/views/posts/help.blade.php
@@ -4,32 +4,27 @@
 @extends('layouts.mainPage')
 @section('MainPage')
 
-<div style="width:68%; margin:0 auto; background-color:#F3F3F3;">
-    <nav class=" flex items-center">
-        <img style="margin:auto" src=" your.png" alt="">
-    </nav>
-    <div class="container" ; style=" align-items: left; margin-left:10%;">
-        <br>
-        <h1>Prekių filtras</h1>
-        <h2>Kategorija</h2>
-
-        <div class="group">
-            <input type="checkbox" id="drama" />
-            <label for="drama">Drama </label>
-        </div>
-
-        <div class="group">
-            <input type="checkbox" id="tikros" />
-            <label for="tikros">Tikros istorijos </label>
-        </div>
-
-        <div class="group">
-            <input type="checkbox" id="meilės" />
-            <label for="meilės">Meilės romanai </label>
-        </div>
-
+<div style="width:68%; margin:0 auto; ">
+    <div class="help">
+        <b>•	Kas yra Pravalas.lt?</b>
+        <p>Tai internetinė svetainė, kurioje gali būti perkamos ir parduodamos knygos.</p>
+        <b>•	Kaip įrašyti kelių autorių vardus ir pavardes?</b>
+        <p>Jeigu norite įvesti autorių kolektyvą, pvz., Viktorijos Daujotytės, Regimanto Tamošaičio, Ritos Tūtlytės ir Giedriaus Viliūno parengtą knygą „Literatūra: vadovėlis 11-12 klasei“, Jūs turite laukelyje „Pavardė“ eilės tvarka įrašyti visas autorių pavardes, t.y. „Daujotytė, Tamošaitis, Tūtlytė, Viliūnas“, o laukelyje „Vardas“ atitinkama eilės tvarka – pirmąsias autorių vardų raides, t.y. „V., R., R., G“. Tokiu būdu eilutėje „Autorius“ atsiras įrašas: „Daujotytė V., Tamošaitis R., Tūtlytė R., Viliūnas G.“</p>
+        <b>•	Kaip įvertinti knygos būklę?</b>
+        <p>- Ideali: visiškai nauja, neskaityta, be defektų (trūkstamų lapų ir pan.), viršelio ir popieriaus pažeidimų (įbrėžimų, raukšlių, įplyšimų).
+             - Labai gera: be defektų, viršelio ir popieriaus pažeidimų, marginalijų paraštėse ar pažymėtų teksto vietų; minimalus susidėvėjimas.
+             - Vidutinė: be defektų, minimalūs viršelio ir popieriaus pažeidimai, nežymiai susidėvėjęs įrišimas, be marginalijų paraštėse ar pažymėtų teksto vietų (nebent pieštuku).
+             - Patenkinama: viršelio ir popieriaus pažeidimai, nežymiai susidėvėjęs įrišimas, galimos marginalijos paraštėse ar pažymėtos teksto vietos (tušinuku).
+             - Bloga: žymūs viršelio ir popieriaus pažeidimai, stipriai susidėvėjęs įrišimas, galimos marginalijos paraštėse ar pažymėtos teksto vietos (tušinuku), trūksta lapų.</p>    
+             <b>•	Kaip supakuoti knygas?</b>
+             <p>Dauguma nedidelių knygų telpa į standartinius A5/A4 tipo vokus ir yra siunčiamos kaip laškai. Jei knyga plona, rekomenduojane įvynioti į papildoma apsauginę plėvelę ar laikraštį. Siunčiant daugiau knygų, pakuoti turėtumėte kaip siuntinius - dėti į dėžes.</p>
+        <b>•	Kaip susisiekti su pardavėju?</b>
+        <p>Jeigu pardavėjas viešai nurodo savo telefono numerį ar elektroninio pašto adresą (šie duomenys atsiranda paspaudus interaktyvų vartotojo vardą), susisiekite su juo jums patogiu būdu.</p>
+        <b>•	Kaip pirkti?</b>
+        <p>Spauskite pridėti į krepšelį, o nuėję ten galėsite pereiti prie apmokėjimo lango, kur turėsite nurodyti mokėjimo ir pristatymo duomenis.</p>
+        <b>•	Kaip įdėti knygą į pardavimą?</b>
+        <p>Jums reiks užsiregistruoti, tada puslapio viršuje matysite mygtuką įdėti knygą, kurį paspaudę būsite nukreipti į knygos įdėjimo langą. Ten reiks įvesti visą informaciją apie ją bei įkelti jos nuotraukas ir parašyti kainą.</p>
         
-       
         
     </div>
 </div>
