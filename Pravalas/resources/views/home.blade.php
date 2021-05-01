8f848db3d06eed82d00c20b03c0fd359fa54dd5f
Virmantas <69695184+Virmantas@users.noreply.github.com>
2021-05-01 23:20:10 +0300

    Update home.blade.php

--- Pravalas/resources/views/home.blade.php
+++ Pravalas/resources/views/home.blade.php
@@ -14,75 +14,77 @@
     <div class="container flex justify-between" style=" align-items: left; margin-left:10%;">
 
 
-        </div>
-        <div class="flex justify-between">
-            <div>
-                @if ($books->count())
-                    @foreach ($books as $book)
+    </div>
+    <div class="flex justify-between">
+        <div>
+            @if ($books->count())
+                @foreach ($books as $book)
 
                     <body>
                         <div class="wrapper1">
-                          <div class="product-img">
-                            <img src="{{ asset('/images/' . $book->image) }}" height="380" width="294">
-                           
-                          </div>
-                          <div class="product-info">
-                            <div class="product-text">
-                                @if (! $book->likedBy( auth()->user() ) )
-                                <form action="{{ route('likes', $book->id) }}" method="post" class="like">
-                                    @csrf
-                                    <button type="submit" class="">
-                                        <img src="unlike.png">   
-                                    </button>
-                                </form>
-                                @else
-                                <form action="{{ route('likes', $book->id) }}" method="post" class="like">
-                                    @csrf
-                                    <button type="submit" class="">
-                                    <img src="like.png">
-                                    </button>
-                                </form>
-                                
-                                @endif
-                              <h1>{{ $book->name }}</h1>
-                              
-                              <h2>{{ $book->author }}</h2>
-                              <h3>{{ $book->user->name }}</h3>
-                              <h3>Knygos Kategorija: {{ $book->category }}</h3>
-                              <h3>Knygos būklė: {{ $book->status }}</h3>
+                            <div class="product-img">
+                                <img src="{{ asset('/images/' . $book->image) }}" height="380" width="294">
+
                             </div>
-                            <div class="product-price-btn">
-                              <p><span>{{ $book->price }}</span> €</p>
-                              <button type="button">buy now</button>
+                            <div class="product-info">
+                                <div class="product-text">
+                                    @auth
+                                        @if (!$book->likedBy(auth()->user()))
+                                            <form action="{{ route('likes', $book->id) }}" method="post" class="like">
+                                                @csrf
+                                                <button type="submit" class="">
+                                                    <img src="unlike.png">
+                                                </button>
+                                            </form>
+                                        @else
+                                            <form action="{{ route('likes', $book->id) }}" method="post" class="like">
+                                                @csrf
+                                                <button type="submit" class="">
+                                                    <img src="like.png">
+                                                </button>
+                                            </form>
+
+                                        @endif
+                                    @endauth
+                                    <h1>{{ $book->name }}</h1>
+
+                                    <h2>{{ $book->author }}</h2>
+                                    <h3>{{ $book->user->name }}</h3>
+                                    <h3>Knygos Kategorija: {{ $book->category }}</h3>
+                                    <h3>Knygos būklė: {{ $book->status }}</h3>
+                                </div>
+                                <div class="product-price-btn">
+                                    <p><span>{{ $book->price }}</span> €</p>
+                                    <button type="button">buy now</button>
+                                </div>
                             </div>
-                          </div>
                         </div>
-                      
-                      </body>
-                        <!---  reikia graziai kad atrodytu cia rodo visas knygas -->
-                       
-                        @auth
-                            <!---  reikia graziai kad atrodytu cias paspausti kad patiko ar ne -->
-                            <div class="flex items-center text-white">
-                                
-                                
 
-                               
+                    </body>
+                    <!---  reikia graziai kad atrodytu cia rodo visas knygas -->
 
-                               
-                            </div>
+                    @auth
+                        <!---  reikia graziai kad atrodytu cias paspausti kad patiko ar ne -->
+                        <div class="flex items-center text-white">
 
-                        @endauth
-                                   
-                    @endforeach
-                    {{ $books->links() }}
-                @else
-                    Sąrašas yra tuščias
-                @endif
-            </div>
 
+
+
+
+
+                        </div>
+
+                    @endauth
+
+                @endforeach
+                {{ $books->links() }}
+            @else
+                Sąrašas yra tuščias
+            @endif
         </div>
+
     </div>
+</div>
 </div>
 </body>
 @endsection
